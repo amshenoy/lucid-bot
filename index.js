631e449c13d4be91ff5397eb77da6035f2510b49
amshenoy <abmshenoy@gmail.com>
2016-07-10 15:44:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,22 +15,23 @@ restService.post('/hook', function (req, res) {
     console.log('hook request');
 
     try {
-        var speech = 'empty speech';
-
         if (req.body) {
             var requestBody = req.body;
 
             if (requestBody.result) {
-                speech = '';
-
-                if (requestBody.result.fulfillment) {
-                    speech = requestBody.result.parameters;
+                //speech = '';
+                //if (requestBody.result.fulfillment) {
+                    //speech = requestBody.result.parameters;
                     //speech = requestBody.result.fulfillment.speech;
-                    speech += ' ';
-                }
+                    //speech += ' ';
+                //}
 
-                if (requestBody.result.action) {
-                    speech = 'action: ' + requestBody.result.action;
+                //if (requestBody.result.action) {
+                    //speech = 'action: ' + requestBody.result.action;
+                //}
+                
+                if (requestBody.result.action == "lucid_location") {
+                    speech = 'lucid location';
                 }
             }
         }
