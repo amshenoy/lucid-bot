626df7692ce4c4e1f63ebdd68f3c8542566f4785
amshenoy <abmshenoy@gmail.com>
2016-07-10 16:14:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ restService.post('/hook', function (req, res) {
             if (requestBody.result) {
                 speech = '';
                 if (requestBody.result.fulfillment) {
-                    speech = requestBody.result.parameters;
                     speech = requestBody.result.fulfillment.speech;
                     speech += ' ';
                 }
