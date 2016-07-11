35558421a1383f21e44483b405613a6a755600b1
amshenoy <abmshenoy@gmail.com>
2016-07-11 08:25:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ restService.post('/hook', function (req, res) {
         return res.json({
             speech: speech,
             displayText: speech,
+            data: {"facebook": {"text": "hello world"}},
             source: 'lucid-cern'
         });
     } catch (err) {
