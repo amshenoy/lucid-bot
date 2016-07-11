c855e65ea0d800548990a4d8f01ed3f1eb9f07c7
amshenoy <abmshenoy@gmail.com>
2016-07-11 08:00:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,39 @@ const bodyParser = require('body-parser');
 const restService = express();
 restService.use(bodyParser.json());
 
+
+messageData = {
+	"attachment": {
+		"type": "template",
+		"payload": {
+			"template_type": "generic",
+			"elements": [{
+				"title": "First card",
+				"subtitle": "Element #1 of an hscroll",
+				"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+				"buttons": [{
+					"type": "web_url",
+					"url": "https://www.messenger.com",
+					"title": "web url"
+				}, {
+					"type": "postback",
+					"title": "Postback",
+					"payload": "Payload for first element in a generic bubble",
+				}],
+			}, {
+				"title": "Second card",
+				"subtitle": "Element #2 of an hscroll",
+				"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+				"buttons": [{
+					"type": "postback",
+					"title": "Postback",
+					"payload": "Payload for second element in a generic bubble",
+				}],
+			}]
+		}
+	}
+}
+	
 restService.post('/hook', function (req, res) {
 
     console.log('hook request');
@@ -49,7 +82,7 @@ restService.post('/hook', function (req, res) {
             speech: speech,
             displayText: speech,
             source: 'lucid-cern'
-            //"data": {"facebook": {'<facebook_message>'}},
+            "data": {"facebook": messageData},
         });
     } catch (err) {
         console.error("Can't process request", err);
